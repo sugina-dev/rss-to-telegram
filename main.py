573a9d3440f21fab99d42a494dd63815422bba1a
sgalal <ayaka@mail.shn.hk>
2020-04-26 22:48:43 +0800

    Handle upload error

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 # _*_ coding: utf-8 _*_
 
 from bs4 import BeautifulSoup
-from datetime import datetime
 from config import config
+from datetime import datetime
 import feedparser
-import telebot
 import json
 import os
+import telebot
+import traceback
+import sys
 
-
-def get_timestamp(dt):
-    return datetime.strptime(dt, '%a, %d %b %Y %H:%M:%S %z').timestamp()
-
+def get_time(dt):
+    return datetime.strptime(dt, '%a, %d %b %Y %H:%M:%S %z')
 
 bot = telebot.TeleBot(config.get('telegram-token'))
 
@@ -23,7 +23,7 @@ else:
     for feed in config.get('feeds'):
         feeds.append(feedparser.parse(feed))
 
-new_posts = []
+new_posts_and_guid = []
 
 if not os.path.exists('posts.json'):
     with open('posts.json', 'w') as f:
@@ -39,22 +39,28 @@ for feed in feeds:
         content = entry['description']
         post = {}
         soup = BeautifulSoup(content, 'html.parser')
-        if soup.img:
-            post['image'] = soup.img['src']
+        #if soup.img:
+        #    post['image'] = soup.img['src']
         post['text'] = '\n'.join(soup.stripped_strings)
-        post['text'] += '\n\n{}'.format(entry['link'])
-        post['date'] = get_timestamp(entry['published'])
+        #post['text'] += '\n{}'.format(entry['link'])
+        post['date'] = get_time(entry['published'])
+        post['text'] += post['date'].strftime('\n%Y-%m-%d %H:%M:%S %Z')
 
-        if entry['guid'] not in guids:
+        this_guid = entry['guid']
+        if this_guid not in guids:
             # TODO: strip guid with a regex like /d{4,}\/{?}$/
-            new_posts.append(post)
-            guids.append(entry['guid'])
+            new_posts_and_guid.append((post, this_guid))
 
-new_posts.sort(key=lambda x: x['date'])
+new_posts_and_guid.sort(key=lambda a_b: a_b[0]['date'])
 
-for post in new_posts:
-    bot.send_message(config.get('channel-id'), post.get('text'))
-
-with open('posts.json', 'w') as f:
-    json.dump(guids, f)
-    f.close()
+try:
+    for post, guid in new_posts_and_guid:
+        bot.send_message(config.get('channel-id'), post.get('text'), disable_web_page_preview=True)
+        guids.append(guid)
+except:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        traceback.print_exception(exc_type, exc_value, exc_traceback, file=sys.stderr)
+finally:
+    with open('posts.json', 'w') as f:
+        json.dump(guids, f)
+        f.close()
